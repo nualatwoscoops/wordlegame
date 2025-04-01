2d28c477e96151acf002f616ac09468f1b96c2d9
20145955 <20145955@tafe.wa.edu.au>
2025-04-01 19:33:23 +0800

    Tet case 1 part b q3

--- scoreguess.py
+++ scoreguess.py
@@ -1,11 +1,19 @@
 #Nuala Malone 2014555 01/04/2025
 
-guess = input("What is your guess?")
-target = "OCEAN"
+#guess = input("What is your guess?")
+#target = "OCEAN"
+
+guess = 'world'
+print("Guess:", guess)
+target = 'world'
+print("Target:", target)
 
 def score_guess(guess, target):
     score = [0, 0, 0, 0, 0]
     if guess == target:
         score = [2, 2, 2, 2, 2]
-        return score
-print(score_guess(guess, target))
+    return score
+
+result = score_guess(guess, target)
+print("Got:", score_guess(guess, target))
+print("Expected:", [2, 2, 2, 2, 2])
